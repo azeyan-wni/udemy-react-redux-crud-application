9bb6decd221b38d9ca5a2961be4a90504a2ad641
azeyan-wni <azeyan@wni.com>
2020-07-16 11:54:16 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div><h1>Hello World!!</h1></div>
-  );
+class App extends Component {
+  render() {
+    return React.createElement(
+      'h1',
+      null,
+      'Hello world!!'
+    );
+  }
 }
 
 export default App;
