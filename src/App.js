c9720322bbd7a8a471ade0e69ea5e59722a3b02b
azeyan-wni <azeyan@wni.com>
2020-07-16 12:13:52 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return React.createElement(
-      'h1',
-      null,
-      'Hello world!!'
-    );
+    return (
+      <React.Fragment>
+        <label htmlFor='bar'>bar</label>
+        <input type='text' onChange={() => {console.log("I am clicked!")}} />
+      </React.Fragment>
+    )
   }
 }
 
