6467c7144680120f7f453a52e7baeaac24fcdbff
azeyan-wni <azeyan@wni.com>
2020-07-20 15:25:23 +0900

    create table

---
+++ src/components/events_index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { readEvents } from '../actions'
+import _ from 'lodash'
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents()
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, event=> (
+      <tr key={event.id}>
+        <th>{event.id}</th>
+        <th>{event.title}</th>
+        <th>{event.body}</th>
+      </tr>
+    ))
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderEvents()}
+          </tbody>
+        </table>
+      </React.Fragment>
+    
+    )
+  } 
+}
+
+const mapStateToProps = state => ({ events: state.events})
+const mapDispatchToProps =({ readEvents })
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
